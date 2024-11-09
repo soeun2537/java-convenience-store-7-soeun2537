a9ab7e520342ff0430c6d8fcc8c3c65e28a88a83
soeun2537 <soeun2537@gmail.com>
2024-11-10 08:59:25 +0900

    test: REGULAR_PRICE_PAYMENT 상태 설정하는 단위 테스트 추가

--- src/test/java/store/dto/server/StatusDtoTest.java
+++ src/test/java/store/dto/server/StatusDtoTest.java
@@ -24,4 +24,20 @@ class StatusDtoTest {
         assertThat(statusDto.getProductName()).isEqualTo("콜라");
         assertThat(statusDto.getQuantity()).isEqualTo(2);
     }
+
+    @Test
+    @DisplayName("REGULAR_PRICE_PAYMENT 상태 설정")
+    void setRegularStatusPaymentStatus() {
+        // given
+        Product product = Product.of("콜라", 1000, "탄산2+1");
+        Integer quantity = 11;
+
+        // when
+        StatusDto statusDto = StatusDto.setRegularStatusPaymentStatus(product, quantity);
+
+        // then
+        assertThat(statusDto.getStatus()).isEqualTo(Status.REGULAR_PRICE_PAYMENT);
+        assertThat(statusDto.getProductName()).isEqualTo("콜라");
+        assertThat(statusDto.getQuantity()).isEqualTo(11);
+    }
 }
