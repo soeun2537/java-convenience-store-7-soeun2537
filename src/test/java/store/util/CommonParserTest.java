da66f2a077930a77bc1f20a1e8447aaf7fd21eed
soeun2537 <soeun2537@gmail.com>
2024-11-09 17:29:42 +0900

    test: 문자열을 분리하는지 확인하는 단위 테스트 추가

--- src/test/java/store/util/CommonParserTest.java
+++ src/test/java/store/util/CommonParserTest.java
@@ -2,6 +2,7 @@ package store.util;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,5 +19,21 @@ class CommonParserTest {
 
         // then
         assertThat(replacedInput).isEqualTo("콜라-3");
+    }
+
+    @Test
+    @DisplayName("문자열을 분리하는지 확인")
+    void separateBySeparator() {
+        // given
+        String input = "콜라-3";
+        String separator = "-";
+
+        // when
+        List<String> separatedInput = CommonParser.separateBySeparator(input, separator);
+
+        // then
+        assertThat(separatedInput).hasSize(2);
+        assertThat(separatedInput.get(0)).isEqualTo("콜라");
+        assertThat(separatedInput.get(1)).isEqualTo("3");
     }
 }
