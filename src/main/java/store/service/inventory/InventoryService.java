d31c0be1d9db6d7aff9fc238a2e24420c1511fac
soeun2537 <soeun2537@gmail.com>
2024-11-09 23:46:17 +0900

    feat: 파일을 통해 재고 설정하는 로직 추가

--- src/main/java/store/service/inventory/InventoryService.java
+++ src/main/java/store/service/inventory/InventoryService.java
@@ -3,9 +3,11 @@ package store.service.inventory;
 import java.time.LocalDate;
 import java.util.List;
 import store.dto.request.file.PromotionRequest;
+import store.dto.request.file.StockRequest;
 import store.model.PromotionManager;
 import store.model.StockManager;
 import store.model.domain.Promotion;
+import store.model.domain.Stock;
 import store.util.MarkdownFileParser;
 import store.util.MarkdownFileReader;
 
@@ -28,6 +30,15 @@ public class InventoryService {
         }
     }
 
+    public void setupStocks(String resourcePath) {
+        List<String> productLines = MarkdownFileReader.readFile(resourcePath);
+        for (String productLine : productLines) {
+            StockRequest stockRequest = MarkdownFileParser.readStockFile(productLine);
+            Stock stock = createStock(stockRequest);
+            stockManager.addStock(stock);
+        }
+    }
+
     private Promotion createPromotion(PromotionRequest promotionRequest) {
         String name = promotionRequest.getName();
         Integer purchaseConditionCount = promotionRequest.getRequiredCount();
@@ -36,5 +47,14 @@ public class InventoryService {
         LocalDate endDate = promotionRequest.getEndDate();
 
         return Promotion.of(name, purchaseConditionCount, giftCount, startDate, endDate);
+    }
+
+    private Stock createStock(StockRequest stockRequest) {
+        String name = stockRequest.getName();
+        Integer price = stockRequest.getPrice();
+        Integer quantity = stockRequest.getQuantity();
+        String promotionName = stockRequest.getPromotionName();
+
+        return Stock.of(name, price, quantity, promotionName);
     }
 }
