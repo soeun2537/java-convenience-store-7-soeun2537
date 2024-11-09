08d67fc05f993e54bfee6997550a711ca06bae83
soeun2537 <soeun2537@gmail.com>
2024-11-09 19:30:45 +0900

    feat: 시작 안내 출력 로직 추가

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,6 +1,12 @@
 package store.view;
 
+import static store.constant.OutputMessage.*;
+
 public class OutputView {
+
+    public static void printStartGuidance() {
+        println(START_GUIDANCE.getMessage());
+    }
 
     private static void println(String content) {
         System.out.println(content);
