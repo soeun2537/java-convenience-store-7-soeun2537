b2e93816d9d615b37f96f3a5850f5b5877ba9794
soeun2537 <soeun2537@gmail.com>
2024-11-09 09:39:01 +0900

    feat: 추가 수량 여부 입력 로직 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,12 +1,17 @@
 package store.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import store.dto.request.input.AddingQuantityStatusRequest;
 import store.dto.request.input.PurchaseProductsRequest;
 
 public class InputView {
 
     public static PurchaseProductsRequest readPurchaseProducts() {
         return PurchaseProductsRequest.from(input());
+    }
+
+    public static AddingQuantityStatusRequest readAddingQuantityStatus() {
+        return AddingQuantityStatusRequest.from(input());
     }
 
     private static String input() {
