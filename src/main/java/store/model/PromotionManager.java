6f96242b7f06c98f0881c911a0fa47b3d404c6c2
soeun2537 <soeun2537@gmail.com>
2024-11-09 22:36:05 +0900

    feat: PromotionManager 클래스 및 싱글톤 인스턴스 생성 로직 추가

---
+++ src/main/java/store/model/PromotionManager.java
@@ -0,0 +1,22 @@
+package store.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import store.model.domain.Promotion;
+
+public class PromotionManager {
+
+    private List<Promotion> promotions;
+
+    private PromotionManager() {
+        this.promotions = new ArrayList<>();
+    }
+
+    private static class PromotionManagerHolder {
+        private static final PromotionManager INSTANCE = new PromotionManager();
+    }
+
+    public static PromotionManager getInstance() {
+        return PromotionManagerHolder.INSTANCE;
+    }
+}
