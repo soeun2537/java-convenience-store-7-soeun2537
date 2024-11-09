618497ea420bfc90d3d51650624f1d8d14474808
soeun2537 <soeun2537@gmail.com>
2024-11-09 09:39:42 +0900

    feat: 정가 결제 여부 입력 로직 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -3,6 +3,7 @@ package store.view;
 import camp.nextstep.edu.missionutils.Console;
 import store.dto.request.input.AddingQuantityStatusRequest;
 import store.dto.request.input.PurchaseProductsRequest;
+import store.dto.request.input.RegularPricePaymentStatusRequest;
 
 public class InputView {
 
@@ -12,6 +13,10 @@ public class InputView {
 
     public static AddingQuantityStatusRequest readAddingQuantityStatus() {
         return AddingQuantityStatusRequest.from(input());
+    }
+
+    public static RegularPricePaymentStatusRequest readRegularPricePaymentStatus() {
+        return RegularPricePaymentStatusRequest.from(input());
     }
 
     private static String input() {
