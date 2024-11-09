fc3bae6a60603cb6514c3db43c068c62984a01e5
soeun2537 <soeun2537@gmail.com>
2024-11-09 09:37:28 +0900

    feat: 구매할 상품 입력 로직 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,8 +1,13 @@
 package store.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import store.dto.request.input.PurchaseProductsRequest;
 
 public class InputView {
+
+    public static PurchaseProductsRequest readPurchaseProducts() {
+        return PurchaseProductsRequest.from(input());
+    }
 
     private static String input() {
         return Console.readLine();
