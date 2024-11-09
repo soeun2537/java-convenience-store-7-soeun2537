f5f2949e61c5a2fe1337c119260da517bc800535
soeun2537 <soeun2537@gmail.com>
2024-11-09 21:00:03 +0900

    test: 재고 Request DTO 단위 테스트 추가

---
+++ src/test/java/store/dto/request/file/StockRequestTest.java
@@ -0,0 +1,25 @@
+package store.dto.request.file;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class StockRequestTest {
+
+    @Test
+    @DisplayName("재고 Request DTO 생성 - 성공 테스트")
+    void from() {
+        // given
+        String line = "콜라,1000,10,탄산2+1";
+
+        // when
+        StockRequest request = StockRequest.from(line);
+
+        // then
+        assertThat(request.getName()).isEqualTo("콜라");
+        assertThat(request.getPrice()).isEqualTo(1000);
+        assertThat(request.getQuantity()).isEqualTo(10);
+        assertThat(request.getPromotionName()).isEqualTo("탄산2+1");
+    }
+}
