b9d6dc77d59fc9a4a8dde912919e1c17d8cb049f
soeun2537 <soeun2537@gmail.com>
2024-11-09 22:37:33 +0900

    test: Promotion 추가 및 조회 단위 테스트 추가

--- src/test/java/store/model/PromotionManagerTest.java
+++ src/test/java/store/model/PromotionManagerTest.java
@@ -2,8 +2,11 @@ package store.model;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.time.LocalDate;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import store.model.domain.Promotion;
 
 class PromotionManagerTest {
 
@@ -16,5 +19,26 @@ class PromotionManagerTest {
 
         // then
         assertThat(instance1).isSameAs(instance2);
+    }
+
+    @Test
+    @DisplayName("Promotion 추가 및 조회")
+    void addAndFindPromotion_succes() {
+        // given
+        PromotionManager manager = PromotionManager.getInstance();
+        Promotion promotion = Promotion.of("탄산2+1", 2, 1,
+                LocalDate.of(2024, 1, 1), LocalDate.of(2024, 12, 31));
+
+        // when
+        manager.addPromotion(promotion);
+        Optional<Promotion> findPromotion = manager.findPromotion("탄산2+1");
+
+        // then
+        assertThat(findPromotion).isPresent();
+        assertThat(findPromotion.get().getName()).isEqualTo("탄산2+1");
+        assertThat(findPromotion.get().getRequiredCount()).isEqualTo(2);
+        assertThat(findPromotion.get().getGiftCount()).isEqualTo(1);
+        assertThat(findPromotion.get().getStartDate()).isEqualTo(LocalDate.of(2024, 1, 1));
+        assertThat(findPromotion.get().getEndDate()).isEqualTo(LocalDate.of(2024, 12, 31));
     }
 }
