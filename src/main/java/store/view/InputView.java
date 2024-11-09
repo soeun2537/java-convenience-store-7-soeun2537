42cd436e79afea8b2c25e60c6dd2d11107732e59
soeun2537 <soeun2537@gmail.com>
2024-11-09 09:36:42 +0900

    feat: 공통 입력 로직 추가

---
+++ src/main/java/store/view/InputView.java
@@ -0,0 +1,10 @@
+package store.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static String input() {
+        return Console.readLine();
+    }
+}
