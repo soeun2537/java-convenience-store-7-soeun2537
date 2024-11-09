758ab51f4fb5a20aacc7755fe10d8372119a9723
soeun2537 <soeun2537@gmail.com>
2024-11-09 17:31:20 +0900

    feat: 문자열을 분리하는 로직 추가

--- src/main/java/store/util/CommonParser.java
+++ src/main/java/store/util/CommonParser.java
@@ -2,6 +2,9 @@ package store.util;
 
 import static store.constant.InputConstant.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CommonParser {
 
     private CommonParser() {
@@ -9,5 +12,9 @@ public class CommonParser {
 
     public static String replaceSquareBrackets(String input) {
         return input.replaceAll(SQUARE_BRACKETS_PATTERN.getContent(), EMPTY_STRING.getContent());
+    }
+
+    public static List<String> separateBySeparator(String input, String separator) {
+        return Arrays.asList(input.split(separator));
     }
 }
