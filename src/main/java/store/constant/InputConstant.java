ac30af907362442e47e352f4ed0a352fea47557f
soeun2537 <soeun2537@gmail.com>
2024-11-09 18:36:00 +0900

    feat: InputConstant 상수 추가

---
+++ src/main/java/store/constant/InputConstant.java
@@ -0,0 +1,23 @@
+package store.constant;
+
+public enum InputConstant {
+
+    SQUARE_BRACKETS_PATTERN("[\\[\\]]"),
+    NUMERIC_PATTERN("\\d+"),
+    YES_NO_PATTERN("[YyNn]"),
+    DATE_PATTERN("^\\d{4}-\\d{2}-\\d{2}$"),
+    DATE_SEPARATOR("-"),
+    TRUE_STRING("Y"),
+    EMPTY_STRING(""),
+    ;
+
+    private final String content;
+
+    InputConstant(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
