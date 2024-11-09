9ceef3bf217812880e36a2acf039746ec2ed46da
soeun2537 <soeun2537@gmail.com>
2024-11-09 19:31:30 +0900

    feat: 보유 재고 출력 로직 추가

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -2,10 +2,31 @@ package store.view;
 
 import static store.constant.OutputMessage.*;
 
+import java.util.List;
+import store.dto.response.StocksResponse;
+import store.dto.response.StocksResponse.InnerStockResponse;
+
 public class OutputView {
 
     public static void printStartGuidance() {
         println(START_GUIDANCE.getMessage());
+    }
+
+    public static void printStocks(StocksResponse response) {
+        println(EXISTING_STOCKS_GUIDANCE.getMessage());
+        printNewLine();
+        printStockDetails(response.getStocksResponse());
+    }
+
+    private static void printStockDetails(List<InnerStockResponse> stockResponses) {
+        for (InnerStockResponse stockResponse : stockResponses) {
+            println(EXISTING_STOCKS.getMessage(
+                    stockResponse.getName(),
+                    stockResponse.getPrice(),
+                    stockResponse.getQuantity(),
+                    stockResponse.getPromotionName()
+            ));
+        }
     }
 
     private static void println(String content) {
