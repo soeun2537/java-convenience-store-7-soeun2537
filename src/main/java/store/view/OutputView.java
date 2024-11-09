1d774d2795a7b6614ff5965db584251a2bfcab65
soeun2537 <soeun2537@gmail.com>
2024-11-09 19:30:15 +0900

    feat: 공통 출력 로직 추가

---
+++ src/main/java/store/view/OutputView.java
@@ -0,0 +1,12 @@
+package store.view;
+
+public class OutputView {
+
+    private static void println(String content) {
+        System.out.println(content);
+    }
+
+    private static void printNewLine() {
+        System.out.print(System.lineSeparator());
+    }
+}
