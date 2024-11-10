73d9f5d36d876ea6b30827da405a1afe80d49cb4
soeun2537 <soeun2537@gmail.com>
2024-11-11 05:12:21 +0900

    test: 재고 응답을 생성하는 단위 테스트 추가

---
+++ src/test/java/store/service/convenience/ConvenienceServiceTest.java
@@ -0,0 +1,44 @@
+package store.service.convenience;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import store.constant.TestPathConstant;
+import store.dto.response.StocksResponse;
+import store.model.PromotionManager;
+import store.model.ReceiptManager;
+import store.model.StockManager;
+import store.service.inventory.InventoryService;
+
+class ConvenienceServiceTest {
+
+    private ConvenienceService convenienceService;
+    private ReceiptManager receiptManager;
+
+    @BeforeEach
+    void beforeEach() {
+        StockManager stockManager = StockManager.getInstance();
+        PromotionManager promotionManager = PromotionManager.getInstance();
+        receiptManager = new ReceiptManager();
+        convenienceService = new ConvenienceService(promotionManager, stockManager, receiptManager);
+
+        receiptManager.createReceipt();
+        stockManager.clearStocks();
+        InventoryService inventoryService = new InventoryService(promotionManager, stockManager);
+        inventoryService.setupPromotions(TestPathConstant.PROMOTION_FILE_PATH.getPath());
+        inventoryService.setupStocks(TestPathConstant.PRODUCT_FILE_PATH.getPath());
+    }
+
+    @Test
+    @DisplayName("재고 응답을 생성")
+    void createStocksResponse() {
+        // when
+        StocksResponse stocksResponse = convenienceService.createStocksResponse();
+
+        // then
+        assertThat(stocksResponse.getStocksResponse()).isNotEmpty();
+        assertThat(stocksResponse.getStocksResponse().getFirst().getName()).isEqualTo("콜라");
+    }
+}
