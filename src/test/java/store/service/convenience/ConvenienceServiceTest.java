c0f68940cac664d80d1c0dabd855478c3e5d39a9
soeun2537 <soeun2537@gmail.com>
2024-11-11 05:14:52 +0900

    test: 상품 구매 - ADDING_QUANTITY 단위 테스트 추가

--- src/test/java/store/service/convenience/ConvenienceServiceTest.java
+++ src/test/java/store/service/convenience/ConvenienceServiceTest.java
@@ -73,4 +73,20 @@ class ConvenienceServiceTest {
         assertThat(statusDto.getProduct()).isNull();
         assertThat(statusDto.getQuantity()).isEqualTo(0);
     }
+
+    @Test
+    @DisplayName("상품 구매: ADDING_QUANTITY")
+    void purchaseProducts_addingQuantity() {
+        // given
+        String input = "[콜라-2]";
+        PurchaseProductsRequest request = PurchaseProductsRequest.from(input);
+
+        // when
+        StatusDto statusDto = convenienceService.purchaseProducts(request).getFirst();
+
+        // then
+        assertThat(statusDto.getStatus()).isEqualTo(Status.ADDING_QUANTITY);
+        assertThat(statusDto.getProductName()).isEqualTo("콜라");
+        assertThat(statusDto.getQuantity()).isEqualTo(1);
+    }
 }
