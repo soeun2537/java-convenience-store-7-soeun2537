c1aa65cc899b1b747854aec574091f3cdbc6b92d
soeun2537 <soeun2537@gmail.com>
2024-11-11 03:38:28 +0900

    test: 저장되어 있는 중복되지 않는 상품 이름 가져오는 단위 테스트 추가

--- src/test/java/store/model/StockManagerTest.java
+++ src/test/java/store/model/StockManagerTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -75,5 +76,24 @@ class StockManagerTest {
 
         // then
         assertThat(stockManager.getStocks()).isEmpty();
+    }
+
+    @Test
+    @DisplayName("저장되어 있는 중복되지 않는 상품 이름 가져오기")
+    void getProductNames() {
+        // given
+        Stock stock1 = Stock.of("콜라", 1000, 10, "탄산2+1");
+        Stock stock2 = Stock.of("콜라", 1000, 5, "탄산2+1");
+        Stock stock3 = Stock.of("사이다", 1200, 5, "null");
+        stockManager.addStock(stock1);
+        stockManager.addStock(stock2);
+        stockManager.addStock(stock3);
+
+        // when
+        Set<String> productNames = stockManager.getProductNames();
+
+        // then
+        assertThat(productNames).hasSize(2);
+        assertThat(productNames).containsOnly(stock1.getProductName(), stock3.getProductName());
     }
 }
