6da4b33844f09e213c22de5497897b97da5ed94c
soeun2537 <soeun2537@gmail.com>
2024-11-11 01:13:05 +0900

    test: 프로모션 재고 > 요청 수량 - 추가 수량 확인 필요 없음 단위 테스트 추가

---
+++ src/test/java/store/service/convenience/PromotionProcessorTest.java
@@ -0,0 +1,65 @@
+package store.service.convenience;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import store.constant.TestPathConstant;
+import store.dto.server.StatusDto;
+import store.model.PromotionManager;
+import store.model.ReceiptManager;
+import store.model.Status;
+import store.model.StockManager;
+import store.model.domain.Receipt;
+import store.model.domain.Stock;
+import store.service.inventory.InventoryService;
+
+class PromotionProcessorTest {
+
+    private PromotionProcessor promotionProcessor;
+    private StockManager stockManager;
+    private PromotionManager promotionManager;
+    private ReceiptManager receiptManager;
+    private PurchaseTransactionHandler transactionHandler;
+
+    @BeforeEach
+    void beforeEach() {
+        stockManager = StockManager.getInstance();
+        promotionManager = PromotionManager.getInstance();
+        receiptManager = new ReceiptManager();
+        transactionHandler = new PurchaseTransactionHandler(stockManager, promotionManager, receiptManager);
+        promotionProcessor = new PromotionProcessor(transactionHandler, promotionManager);
+
+        stockManager.clearStocks();
+        receiptManager.createReceipt();
+        InventoryService inventoryService = new InventoryService(promotionManager, stockManager);
+        inventoryService.setupPromotions(TestPathConstant.PROMOTION_FILE_PATH.getPath());
+        inventoryService.setupStocks(TestPathConstant.PRODUCT_FILE_PATH.getPath());
+    }
+
+    @Test
+    @DisplayName("프로모션 재고 > 요청 수량: 추가 수량 확인 필요 없음")
+    void handlePromotion_checkAdditionalQuantity() {
+        // given
+        Stock promotionStock = stockManager.findPromotionAndGeneralStocks("콜라").getFirst();
+        Integer quantity = 6;
+
+        // when
+        StatusDto statusDto = promotionProcessor.handlePromotion(promotionStock, quantity);
+
+        // then
+        Receipt receipt = receiptManager.get();
+
+        assertThat(promotionStock.getQuantity()).isEqualTo(4);
+        assertThat(receipt.getPurchasedStocks().size()).isEqualTo(1);
+        assertThat(receipt.getPurchasedStocks().getFirst().getProductName()).isEqualTo(promotionStock.getProductName());
+        assertThat(receipt.getPurchasedStocks().getFirst().getQuantity()).isEqualTo(6);
+        assertThat(receipt.getGiftStocks().size()).isEqualTo(1);
+        assertThat(receipt.getGiftStocks().getFirst().getProductName()).isEqualTo(promotionStock.getProductName());
+        assertThat(receipt.getGiftStocks().getFirst().getQuantity()).isEqualTo(2);
+        assertThat(statusDto.getStatus()).isEqualTo(Status.NO_ACTION_REQUIRED);
+        assertThat(statusDto.getProduct()).isNull();
+        assertThat(statusDto.getQuantity()).isEqualTo(0);
+    }
+}
