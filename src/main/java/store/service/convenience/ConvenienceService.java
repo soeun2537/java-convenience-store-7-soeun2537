97af8a37a61e6c9bccc2b26417e7a0c60aac0108
soeun2537 <soeun2537@gmail.com>
2024-11-11 05:13:02 +0900

    feat: 재고 응답을 생성하는 로직 추가

---
+++ src/main/java/store/service/convenience/ConvenienceService.java
@@ -0,0 +1,30 @@
+package store.service.convenience;
+
+import store.dto.response.StocksResponse;
+import store.model.PromotionManager;
+import store.model.StockManager;
+import store.model.ReceiptManager;
+
+public class ConvenienceService {
+
+    private final StockManager stockManager;
+    private final ReceiptManager receiptManager;
+    private final PromotionProcessor promotionProcessor;
+    private final GeneralProcessor generalProcessor;
+
+    public ConvenienceService(PromotionManager promotionManager,
+                              StockManager stockManager,
+                              ReceiptManager receiptManager
+    ) {
+        this.stockManager = stockManager;
+        this.receiptManager = receiptManager;
+        PurchaseTransactionHandler transactionHandler =
+                new PurchaseTransactionHandler(stockManager, promotionManager, receiptManager);
+        this.promotionProcessor = new PromotionProcessor(transactionHandler, promotionManager);
+        this.generalProcessor = new GeneralProcessor(transactionHandler);
+    }
+
+    public StocksResponse createStocksResponse() {
+        return StocksResponse.from(stockManager.getStocks());
+    }
+}
