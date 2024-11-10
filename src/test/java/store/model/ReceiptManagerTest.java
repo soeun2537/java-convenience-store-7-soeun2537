bcc8c6cf473997e1113f76939b52665d8564e199
soeun2537 <soeun2537@gmail.com>
2024-11-11 03:02:30 +0900

    test: 영수증 가져오기 단위 테스트 추가

--- src/test/java/store/model/ReceiptManagerTest.java
+++ src/test/java/store/model/ReceiptManagerTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import store.model.domain.Product;
 import store.model.domain.Receipt;
 
 class ReceiptManagerTest {
@@ -26,5 +27,24 @@ class ReceiptManagerTest {
         Receipt receipt = receiptManager.get();
         assertThat(receipt.getPurchasedStocks()).isEmpty();
         assertThat(receipt.getGiftStocks()).isEmpty();
+    }
+
+    @Test
+    @DisplayName("영수증 가져오기")
+    void get() {
+        // given
+        receiptManager.createReceipt();
+        Receipt receipt = receiptManager.get();
+        Product product = Product.of("콜라", 1000, "탄산2+1");
+        Integer purchaseQuantity = 3;
+        Integer giftQuantity = 1;
+        receipt.addPurchasedStock(product, purchaseQuantity);
+        receipt.addGiftStock(product, giftQuantity);
+
+        // when
+        Receipt newReceipt = receiptManager.get();
+
+        // then
+        assertThat(newReceipt).isSameAs(receipt);
     }
 }
