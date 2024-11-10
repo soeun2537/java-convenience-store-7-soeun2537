c5111c022ff1fd76932f2b67c9be1ebd9d6fce59
soeun2537 <soeun2537@gmail.com>
2024-11-10 20:55:26 +0900

    feat: 멤버십 최대 한도 상수 추가

--- src/main/java/store/constant/ConvenienceConstant.java
+++ src/main/java/store/constant/ConvenienceConstant.java
@@ -3,6 +3,7 @@ package store.constant;
 public class ConvenienceConstant {
 
     public static final int MAX_RETRIES = 3;
+    public static final int MAX_MEMBERSHIP_DISCOUNT = 8000;
     public static final double MEMBERSHIP_DISCOUNT_RATE = 0.3;
     public static final String NO_PROMOTION = "null";
     public static final String EMPTY_STRING = "";
