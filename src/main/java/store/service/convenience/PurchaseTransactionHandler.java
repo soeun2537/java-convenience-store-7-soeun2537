2b3d4e89f10bf4868e3ce6e33798501fe2966d5f
soeun2537 <soeun2537@gmail.com>
2024-11-10 21:57:43 +0900

    feat: 프로모션 포함 상품 구매 - 증정 계산 로직 추가

---
+++ src/main/java/store/service/convenience/PurchaseTransactionHandler.java
@@ -0,0 +1,40 @@
+package store.service.convenience;
+
+import static store.constant.message.ErrorMessage.*;
+
+import store.model.PromotionManager;
+import store.model.StockManager;
+import store.model.domain.Product;
+import store.model.domain.Promotion;
+import store.model.domain.Receipt;
+import store.model.ReceiptManager;
+
+public class PurchaseTransactionHandler {
+
+    private final StockManager stockManager;
+    private final PromotionManager promotionManager;
+    private final ReceiptManager receiptManager;
+
+    public PurchaseTransactionHandler(StockManager stockManager,
+                                      PromotionManager promotionManager,
+                                      ReceiptManager receiptManager
+    ) {
+        this.stockManager = stockManager;
+        this.promotionManager = promotionManager;
+        this.receiptManager = receiptManager;
+    }
+
+    public void processWithGift(Product product, Integer quantity, boolean calculateGift) {
+        Receipt receipt = receiptManager.get();
+        Promotion promotion = promotionManager.findPromotion(product.getPromotionName())
+                .orElseThrow(() -> new IllegalArgumentException(NOT_FOUND_PROMOTION.getMessage()));
+
+        stockManager.reduceStockQuantity(product, quantity);
+        receipt.addPurchasedStock(product, quantity);
+        if (calculateGift) {
+            receipt.addGiftStock(product, (quantity / (promotion.getRequiredPlusGiftCount())));
+            return;
+        }
+        receipt.addGiftStock(product, quantity);
+    }
+}
