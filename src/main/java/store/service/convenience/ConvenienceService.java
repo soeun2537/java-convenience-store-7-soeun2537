6ad09bdde7c1f8373038b64feeb6feacad5c4827
soeun2537 <soeun2537@gmail.com>
2024-11-11 05:19:06 +0900

    feat: 상품 구매 로직 추가

--- src/main/java/store/service/convenience/ConvenienceService.java
+++ src/main/java/store/service/convenience/ConvenienceService.java
@@ -1,8 +1,16 @@
 package store.service.convenience;
 
+import java.util.ArrayList;
+import java.util.List;
+import store.dto.request.input.PurchaseProductsRequest;
+import store.dto.request.input.PurchaseProductsRequest.InnerPurchaseProductRequest;
+import store.dto.response.ReceiptResponse;
 import store.dto.response.StocksResponse;
+import store.dto.server.StatusDto;
 import store.model.PromotionManager;
 import store.model.StockManager;
+import store.model.domain.Stock;
+import store.model.domain.Receipt;
 import store.model.ReceiptManager;
 
 public class ConvenienceService {
@@ -30,5 +38,36 @@ public class ConvenienceService {
 
     public void createReceipt() {
         receiptManager.createReceipt();
+    }
+
+    public List<StatusDto> purchaseProducts(PurchaseProductsRequest purchaseProductsRequest) {
+        List<StatusDto> stockDtos = new ArrayList<>();
+
+        for (InnerPurchaseProductRequest request : purchaseProductsRequest.getPurchaseProductsRequests()) {
+            List<Stock> stocks = stockManager.findPromotionAndGeneralStocks(request.getProductName());
+            stockDtos.add(purchaseProduct(stocks, request.getProductQuantity()));
+        }
+
+        return stockDtos;
+    }
+
+    private StatusDto purchaseProduct(List<Stock> stocks, Integer quantity) {
+        if (stockManager.existsPromotionStock(stocks.getFirst().getProductName())) {
+            return promotionProcessor.handlePromotion(stocks.getFirst(), quantity);
+        }
+        return generalProcessor.handleGeneral(stocks.getFirst(), quantity);
+    }
+
+    public void applyAddingQuantity(StatusDto statusDto) {
+        promotionProcessor.processAddingQuantity(statusDto);
+    }
+
+    public void applyRegularPricePayment(StatusDto statusDto) {
+        promotionProcessor.processRegularPricePayment(statusDto);
+    }
+
+    public void applyMembership() {
+        Receipt receipt = receiptManager.get();
+        receipt.applyMembership();
     }
 }
