2f6ee2da6a4e97d801c588f508fa661fb2896ea3
soeun2537 <soeun2537@gmail.com>
2024-11-11 09:42:53 +0900

    fix: 코드 수정에 따른 테스트 코드 삭제

--- src/test/java/store/model/ReceiptManagerTest.java
+++ src/test/java/store/model/ReceiptManagerTest.java
@@ -5,19 +5,15 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import store.model.domain.Product;
-import store.model.domain.Promotion;
 import store.model.domain.Receipt;
 
 class ReceiptManagerTest {
 
     private ReceiptManager receiptManager;
-    private PromotionManager promotionManager;
 
     @BeforeEach
     void beforeEach() {
         receiptManager = new ReceiptManager();
-        promotionManager = PromotionManager.getInstance();
     }
 
     @Test
@@ -30,25 +26,5 @@ class ReceiptManagerTest {
         Receipt receipt = receiptManager.get();
         assertThat(receipt.getPurchasedStocks()).isEmpty();
         assertThat(receipt.getGiftStocks()).isEmpty();
-    }
-
-    @Test
-    @DisplayName("영수증 가져오기")
-    void get() {
-        // given
-        receiptManager.createReceipt();
-        Receipt receipt = receiptManager.get();
-        Promotion promotion = promotionManager.findPromotion("탄산2+1").get();
-        Product product = Product.of("콜라", 1000, "탄산2+1");
-        Integer purchaseQuantity = 3;
-        Integer giftQuantity = 1;
-        receipt.addPurchasedStock(product, purchaseQuantity, promotion);
-        receipt.addGiftStock(product, giftQuantity, promotion);
-
-        // when
-        Receipt newReceipt = receiptManager.get();
-
-        // then
-        assertThat(newReceipt).isSameAs(receipt);
     }
 }
